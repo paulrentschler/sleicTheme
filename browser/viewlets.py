c2ee89981535f283eb63428849a2030dffaef8e4
Paul Rentschler <paul@rentschler.ws>
2010-03-19 15:42:50 +0000

    Added the customized personal bar and cleaned up the content actions bar.

--- browser/viewlets.py
+++ browser/viewlets.py
@@ -2,6 +2,7 @@ from zope.component import getMultiAdapter
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import ViewletBase
 from plone.app.layout.viewlets import common
+from Products.CMFCore.utils import getToolByName
 
 class LogoViewlet(ViewletBase):
     render = ViewPageTemplateFile('templates/logo.pt')
@@ -26,3 +27,13 @@ class PathBarViewlet(common.PathBarViewlet):
     
 class SiteActionsViewlet(common.SiteActionsViewlet):
     render = ViewPageTemplateFile('templates/site_actions.pt')
+
+class PersonalBarViewlet(common.PersonalBarViewlet):
+    render = ViewPageTemplateFile('templates/personal_bar.pt')
+    
+    def update(self):
+        common.PersonalBarViewlet.update(self)
+        
+        mt = getToolByName(self.context, 'portal_membership')
+        self.canManagePortlets = mt.checkPermission('Portlets: Manage portlets', self.context)
+        self.canManagePortal = mt.checkPermission('Manage portal', self.context)
